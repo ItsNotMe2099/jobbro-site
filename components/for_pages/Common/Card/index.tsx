8ffbad6ee64bff232a0ba73209b8a94495995d10
NOTAPC\NotAUser2000 <sewardwoden@gmail.com>
2023-09-29 18:05:50 +0300

    EditForm

--- components/for_pages/Common/Card/index.tsx
+++ components/for_pages/Common/Card/index.tsx
@@ -1,15 +1,17 @@
 import styles from './index.module.scss'
 import { ReactElement } from 'react'
+import classNames from 'classnames'
 
 interface Props {
   title: string | ReactElement
   children: ReactElement | ReactElement[]
+  className?: string
 }
 
 export default function Card(props: Props) {
 
   return (
-    <div className={styles.root}>
+    <div className={classNames(styles.root, props.className)}>
       <div className={styles.title}>
         {props.title}
       </div>
--- components/for_pages/Lk/YourCompany/Form/Forms/EditForm/index.tsx
+++ components/for_pages/Lk/YourCompany/Form/Forms/EditForm/index.tsx
@@ -7,6 +7,7 @@ import { RequestError } from '@/types/types'
 import { SnackbarType } from '@/types/enums'
 import InputField from '@/components/fields/InputField'
 import Validator from '@/utils/validator'
+import SelectField from '@/components/fields/SelectField'
 
 interface Props {
 
@@ -60,10 +61,27 @@ export default function EditForm(props: Props) {
   return (
     <FormikProvider value={formik}>
       <Form className={styles.form}>
-        <Card title='Details'>
+        <Card className={styles.card} title='Details'>
           <InputField placeholder='Office name' name='officeName' label={formik.values.officeName ? 'Office name' : ''}
             labelType='in'
             validate={Validator.required} />
+          <div className={styles.line}>
+            <SelectField placeholder='Country' className={styles.select} name='country' options={[]} />
+            <InputField className={styles.select} placeholder='Postal code' name='postalCode' label={formik.values.officeName ? 'Postal code' : ''}
+              labelType='in'
+              validate={Validator.required} />
+          </div>
+          <InputField placeholder='City' name='city' label={formik.values.officeName ? 'City' : ''}
+            labelType='in'
+            validate={Validator.required} />
+          <div className={styles.line}>
+            <InputField placeholder='Street name' name='street' label={formik.values.officeName ? 'Street' : ''}
+              labelType='in'
+              validate={Validator.required} />
+            <InputField placeholder='House number' name='houseNumber' label={formik.values.officeName ? 'House number' : ''}
+              labelType='in'
+              validate={Validator.required} />
+          </div>
         </Card>
       </Form>
     </FormikProvider>
