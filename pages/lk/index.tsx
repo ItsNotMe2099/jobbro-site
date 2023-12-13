a7c04a2e2216bb7bf92844a4ca309d69c56bccc0
Andrey <a1081515@yandex.ru>
2023-12-13 08:36:57 +0300

    move lending to index page

---
+++ pages/lk/index.tsx
@@ -0,0 +1,28 @@
+//import styles from './index.module.scss'
+import {getAuthServerSideProps} from '@/utils/auth'
+import {Routes} from '@/types/routes'
+import {ProfileType} from '@/data/enum/ProfileType'
+
+
+export default function IndexPage() {
+  return null
+}
+
+
+export const getServerSideProps = getAuthServerSideProps(null, async (context, user) => {
+  let redirect: string = '/admin'
+  switch (user?.profileType){
+    case ProfileType.Employee:
+      redirect = Routes.lkDashboard
+      break
+    case ProfileType.Hirer:
+      redirect = Routes.lkDashboard
+      break
+  }
+  return {
+    redirect: {
+      permanent: false,
+      destination: redirect
+    }
+  }
+})
