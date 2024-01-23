f8e85253257c44a90db43090191b4340f413cfd3
Andrey <a1081515@yandex.ru>
2024-01-23 18:42:24 +0300

    JBRO-138

--- pages/job/[id]/index.tsx
+++ pages/job/[id]/index.tsx
@@ -1,20 +1,22 @@
 import styles from './index.module.scss'
 import {useRouter} from 'next/router'
 import VacancyRepository from '@/data/repositories/VacancyRepository'
-import { IVacancyWithCurrentUserApply} from '@/data/interfaces/IVacancy'
+import {IVacancyWithCurrentUserApply} from '@/data/interfaces/IVacancy'
 import JobPreview from '@/components/for_pages/Lk/Jobs/JobPreview'
 import {GetServerSidePropsContext, GetServerSidePropsResult} from 'next/types'
 import {CookiesType, ModalType} from '@/types/enums'
 import Layout from '@/components/layout/Layout'
 import FormStickyFooter from '@/components/for_pages/Common/FormStickyFooter'
 import Button from '@/components/ui/Button'
-import { useRef} from 'react'
+import {useRef} from 'react'
 import {useAppContext} from '@/context/state'
 import {ApplicationCreateModalArguments} from '@/types/modal_arguments'
 import useTranslation from 'next-translate/useTranslation'
 import {RequestError} from '@/types/types'
 import ChatOnPage from '@/components/for_pages/Common/ChatOnPage'
 import ApplyForJobForm from '@/components/for_pages/Common/ApplyForJobForm'
+import {useEmployeeAiCvRequestsContext} from '@/context/employee_cv_request_state'
+import {AiRequestStatus} from '@/data/enum/AiRequestStatus'
 
 interface Props {
   job: IVacancyWithCurrentUserApply
@@ -26,6 +28,9 @@ const JobPageInner = (props: Props) => {
   const {isSmDesktopWidth} = appContext.size
   const { t } = useTranslation()
   const ref = useRef<HTMLDivElement | null>(null)
+
+  const employeeAiCvRequests = useEmployeeAiCvRequestsContext()
+  const request = employeeAiCvRequests.requests.length > 0 ? employeeAiCvRequests.requests[0] : null
 
   const openApplicationModal = () => {
     if(isTabletWidth) {
@@ -41,7 +46,7 @@ const JobPageInner = (props: Props) => {
       <div className={styles.root}>
         <div ref={ref} className={styles.container} id='idVacancyContainer'>
           <JobPreview job={props.job} company={props.job.company}/>
-          {!isSmDesktopWidth &&
+          {!isSmDesktopWidth && (request && (!([AiRequestStatus.InQueue, AiRequestStatus.InProgress] as AiRequestStatus[]).includes(request.status) && !(request?.status === AiRequestStatus.Finished && request.vacancyId === props.job.id && !request.cv?.isChecked))) &&
           <FormStickyFooter boundaryElement={'#idVacancyContainer'} formRef={ref} className={styles.footer}>
             <Button spinner={false} type='submit' styleType='large' color='green'
                     onClick={() => openApplicationModal()}>
